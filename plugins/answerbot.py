e234fbf434633b6cfbf63c58e3a6208b9add5fd3
Anthony Nguyen <anknguyen@gmail.com>
2014-10-15 18:18:18 -0400

    Added learning ability

--- plugins/answerbot.py
+++ plugins/answerbot.py
@@ -18,6 +18,8 @@ class AnswerPlugin:
             p = a.split("*")
             self.answers[p[0]] = p[1:]
 
+        self.question = None
+
     def shutdown(self):
         pass
 
@@ -28,19 +30,32 @@ class AnswerPlugin:
 
         return None
 
+    _QUESTIONRE = re.compile("Question \d+( \(.+?\))?: ")
+    _ANSWERRE = re.compile("Time's up! The answer was: (.+)")
     def on_chatmsg(self, ev):
         if ev.source.nick != "triviabot":
             return
 
         message = ev.arguments[0]
 
-        if re.match("Question \d+: ", message) is not None:
-            message = re.sub("Question \d+: ", "", message)
-        else:
+        if message == "Say !trivia ?library? to play again.":
+            self.bot.say("!trivia")
             return
+
+        m = self._ANSWERRE.findall(message)
+        if m and self.question is not None:
+            self.bot.say("Learned: {}".format(m[0].strip()))
+            self.answers[remove_punc(self.question)] = m[:]
+            self.question = None
+            return
+
+        if self._QUESTIONRE.match(message) is not None:
+            message = self._QUESTIONRE.sub("", message)
+            print("Question seen: {}".format(message))
+            self.question = message.strip()
 
         answer = self.get_answer(message)
         if answer is not None:
-            print(answer)
+            self.question = None
             self.bot.say(answer.strip())
         
